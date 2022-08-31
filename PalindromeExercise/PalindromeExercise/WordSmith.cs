bd72e9b7b5c1805b3f2672eab0dca91711cb2488
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-31 10:58:28 -0400

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+
+            if(word == null || word.Length == 0)
+            {
+                return false;
+            }
+
+            string wordLowered = word.ToLower();
+            string backwards = "";
+
+            for(int i = word.Length -1; i >=0; i--)
+            {
+                backwards += wordLowered[i];
+            }
+
+            return wordLowered == backwards;
+
+            //if(word == backwards)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+    }
+}
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTest.cs
@@ -0,0 +1,30 @@
+﻿
+using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTest
+    {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("null", false)]
+
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
